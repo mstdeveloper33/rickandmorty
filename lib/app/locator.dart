052ab2d45cd636c4e1555ec253af090190a9ef1f
MehmetSinanTatar <mstdeveloper@MehmetSinanTatar.local>
2025-01-05 16:45:59 +0300

    yenieklendiyeni

--- lib/app/locator.dart
+++ lib/app/locator.dart
@@ -6,7 +6,7 @@ import '../services/api_service.dart';
 
 //! Burada getit paketini kullanarak servislerimizi register edeceğiz. Yani Api servislerinin çalışmasını sağlayacağız ama her seferinde oluşturmak yerine bir kere oluşturup her yerden erişebileceğimiz bir yapı oluşturacağız. Get burada aslında bir dependency injection paketi. Dependency injection, bir sınıfın başka bir sınıfı kullanabilmesi için ona bir nesne verilmesi işlemidir. Bu sayede sınıflar arasındaki bağımlılıklar azalır ve sınıfların test edilmesi kolaylaşır. GetIt ise bu işlemi yapmamızı sağlayan bir paket.Ayrıca main.dart dosyamızda bu locator.dart dosyasını import etmeyi unutmamamız gerekiyor.
 
-//Dependency injection yapıldı
+//Dependency injection yapıldı güzel bir şey
 final locator = GetIt.instance;
 
  Future <void> setupLocator() async {
